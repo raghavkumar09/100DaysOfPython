0003f0854386eecb3bca90d53280f6bd25dac7c2
Raghav Kumar <jharaghav330@gmail.com>
2021-12-21 16:05:20 +0530

    Add Day 1
    Signed-off-by: Raghav Kumar <jharaghav330@gmail.com>

---
+++ Day_1/Day1_Debugging.py
@@ -0,0 +1,16 @@
+          # It's Debugg Problem
+# print("Day 1 - String Manupulation)
+# print("string concatination is Done with the "+" sign.")
+#    print('eg. print("Hello "+ "World")')
+# print("New Line Created with a backslash and n.")
+
+     # Solution 
+
+
+# print("Day 1 - String Manupulation")
+# print('string concatination is Done with the "+" sign.')
+# print('eg. print("Hello "+ "World")')
+# print("New Line Created with a backslash and n.")
+
+# print("Hello" + input("What is your name?"))
+print(len(input("What is your name?")))
---
+++ Day_1/Final_project.py
@@ -0,0 +1,6 @@
+name = input("Enter your name : \n")
+print("Good Morning " + name )
+
+city = input("Enter the city that they grew up in : ")
+pet = input("Enter your pet name : ")
+print(city + pet)
